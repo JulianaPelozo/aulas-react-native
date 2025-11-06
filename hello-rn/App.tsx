c030ac7c28d06aba7ca4fce80e24a848d3507d6e
JulianaPelozo <julianapelozopacheco@gmail.com>
2025-11-05 21:08:43 -0300

    Update app.tsx

--- hello-rn/App.tsx
+++ hello-rn/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//import styles from './styles'; <--- importando o styles do outro arquivo
 
+//const avatarUrl = "url da imagem"
 export default function App() {
   return (
     <View style={styles.container}>
